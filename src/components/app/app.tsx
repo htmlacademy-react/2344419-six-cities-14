829ee6a8788b6c79180cf8cfc7dff8d582a45b78
TyurinaNataliya <TyurinaNataliya@gmail.com>
2023-10-18 20:36:53 +0300

    Маршрутизация в Арр и страница ошибки

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const.ts';
 import PagesMainContainer from '../../pages/pages-main-container/pages-main-container.tsx';
+import PagesNotFoundContainer from '../../pages/pages-not-found-container/pages-not-found-container.tsx';
+import PagesFavoritesContainer from '../../pages/pages-favorites-container/pages-favorites-container.tsx';
+import PagesLoginContainer from '../../pages/pages-login-container.tsx/pages-login-container.tsx';
+import PagesOfferContainer from '../../pages/pages-offer-container/pages-offer-container.tsx';
 
 type AppProps = {
   placeCartPrice: number;
@@ -7,6 +13,30 @@ type AppProps = {
 
 export default function App({placeCartPrice,countRentalOffers}:AppProps):JSX.Element{
   return(
-    <PagesMainContainer placeCartPrice = {placeCartPrice} countRentalOffers = {countRentalOffers}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<PagesMainContainer placeCartPrice = {placeCartPrice} countRentalOffers = {countRentalOffers}/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<PagesFavoritesContainer />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<PagesLoginContainer />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<PagesOfferContainer />}
+        />
+        <Route
+          path='*'
+          element={<PagesNotFoundContainer />}
+        />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
---
+++ src/pages/pages-not-found-container/pages-not-found-container.tsx
@@ -0,0 +1,15 @@
+
+function PagesNotFoundContainer():JSX.Element{
+  return(
+    <div style={{ marginTop: 100}}>
+      <h1 className="offer__name">
+    404 Not Found
+        <br/>
+        <a className="header__logo-link" href="http://localhost:5173/">
+         Вернуться на главную страницу
+        </a>
+      </h1>
+    </div>
+  );
+}
+export default PagesNotFoundContainer;
