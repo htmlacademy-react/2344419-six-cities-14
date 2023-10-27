867af475b6e32cf4c1b4eeb039e5d124a0958e28
TyurinaNataliya <TyurinaNataliya@gmail.com>
2023-10-27 14:04:39 +0300

    Oкруглила расчет процентов

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -36,7 +36,7 @@ function PagesCard ({offer} :CitesPlacesProps):JSX.Element{
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: `${rating * 20}%`,}}></span>
+            <span style={{width: `${Math.round(rating * 20)}%`,}}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
--- src/components/offer-card.tsx
+++ src/components/offer-card.tsx
@@ -57,7 +57,7 @@ function OfferCard({offer, reviews}:OfferCardProps):JSX.Element{
           </div>
           <div className="offer__rating rating">
             <div className="offer__stars rating__stars">
-              <span style={{width: `${rating * 20}%`}}></span>
+              <span style={{width: `${Math.round(rating * 20)}%`}}></span>
               <span className="visually-hidden">Rating</span>
             </div>
             <span className="offer__rating-value rating__value">{rating}</span>
